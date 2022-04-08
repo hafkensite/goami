0fe45a45f58b0b5e174e03bae391d8a1ea890247
Jasper Hafkenscheid <jasper.hafkenscheid@wearespindle.com>
2022-04-08 14:27:13 +0200

    Add PJSIPShowAors function

--- ami/pjsip.go
+++ ami/pjsip.go
@@ -49,6 +49,11 @@ func PJSIPShowEndpoints(ctx context.Context, client Client, actionID string) ([]
 	return requestList(ctx, client, "PJSIPShowEndpoints", actionID, "EndpointList", "EndpointListComplete")
 }
 
+// PJSIPShowAors list pjsip Aors.
+func PJSIPShowAors(ctx context.Context, client Client, actionID string) ([]Response, error) {
+	return requestList(ctx, client, "PJSIPShowAors", actionID, "AorList", "AorListComplete")
+}
+
 // PJSIPShowRegistrationInboundContactStatuses lists ContactStatuses for PJSIP inbound registrations.
 func PJSIPShowRegistrationInboundContactStatuses(ctx context.Context, client Client, actionID string) ([]Response, error) {
 	return requestList(ctx, client, "PJSIPShowRegistrationInboundContactStatuses", actionID, "ContactStatusDetail", "ContactStatusDetailComplete")
